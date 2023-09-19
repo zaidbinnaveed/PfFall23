4e6b506b3729ae7ac3dd0f0ce57104316579650d
zaidbinnaveed <142867727+zaidbinnaveed@users.noreply.github.com>
2023-09-19 09:02:41 +0500

    Create Task01.c

---
+++ Labs05/Task01.c
@@ -0,0 +1,16 @@
+Question 1;
+#include <stdio.h>
+int main() {
+           int number;
+           printf("Enter your number:");
+           scanf("%d", &number);
+           if(number%3==0)
+           {
+               printf("The number is a multiple of 3");
+           }
+           else
+           {
+               printf("The number is not a multiple of 3");
+           return 0;
+        }
+    }
